553c2e9d98d2d7d1a5050207965264b035843d67
Ravi Hamsa <ravikumar.anantha@yahoo.in>
2017-09-14 09:41:45 +0530

    update

--- src/pages/ValidationRule.js
+++ src/pages/ValidationRule.js
@@ -2,7 +2,7 @@
  * Created by ravi.hamsa on 10/8/16.
  */
 import React, {Component, PropTypes} from "react";
-import {SmartWrapper, RXForm, RXTextInput, addValidationRule} from 'react-starter-components';
+import {SmartWrapper, RXForm, RXTextInput, addValidationRule, RXDropdown} from 'react-starter-components';
 
 addValidationRule('noFloat', function (rule, value) {
     const intValue = parseInt(value);
@@ -26,6 +26,7 @@ export default class ValidationRule extends SmartWrapper {
                 <RXTextInput validations={[{expr: 'number'}, {expr: 'noFloat'}]} name="noFloat" label="No Float" autoFocus
                              />
                 <RXTextInput  validations={[{expr: 'number'}, {expr: 'floatOnly'}]} name="floatOnly" label="Float Only"/>
+                <RXDropdown  name="dropdown" options={[{id:'1', name:'one'}, {id:'2', name:'two'}, {id:'3', name:'two'}, {id:'4', name:'two'}]}  bodyPosition={'up'}/>
             </RXForm>
         </div>
     }
