41d0a9f6dd21e46c2d96bee950b44a0f2b2efdfb
Ravi Hamsa <ravikumar.anantha@yahoo.in>
2017-10-11 10:00:37 +0530

    added input set example

--- src/pages/InputSet.js
+++ src/pages/InputSet.js
@@ -2,26 +2,96 @@
  * Created by ravi.hamsa on 10/8/16.
  */
 import React, {Component, PropTypes} from "react";
-import {SmartWrapper, RXSelectionElement} from 'react-starter-components';
+import {
+    SmartWrapper,
+    RXSelectionElement,
+    RXForm,
+    RXElementGroup,
+    getUniqueId,
+    RXDropdown,
+    RXDropdownItem
+} from 'react-starter-components';
+
+const rxOptions = [{id: '1', name: 'one'}, {id: '2', name: 'two'}, {id: '3', name: 'three'}, {id: '4', name: 'four'}]
 
 
-class RXTextInputSet extends RXSelectionElement {
+class RXDropdownSet extends RXSelectionElement {
+
+
+    constructor(props) {
+        super(props);
+        this.defineListItem();
+        this.changing = false;
+        this.savedValue=this.props.value.split(',');
+    }
+
+    defineListItem() {
+        const setSelectionManager = this.selectionManager;
+        class RXDropdownSetItem extends RXDropdownItem {
+            getClassName() {
+                let {itemData, selectionManager} = this.props;
+                let className = 'list-item ';
+                if (selectionManager.isSelected(itemData)) {
+                    className += ' active';
+                } else if (setSelectionManager.isSelected(itemData)) {
+                    className += ' hidden';
+                }
+                return className;
+            }
+        }
+
+        this.ListItem = RXDropdownSetItem;
+        setTimeout(()=>{
+            this.addOption();
+        },4000)
+    }
+
+    getSelectedOptions() {
+        return this.selectionManager.getSelected();
+    }
+
+    onValueChangeHandler(changed, fullObject) {
+        if (this.changing) {
+            return;
+        }
+        this.changing = true;
+        this.applyValue(Object.values(fullObject).join(','));
+        this.changing = false;
+    }
+
+    addOption(){
+       this.savedValue.push(getUniqueId());
+       this.forceUpdate();
+    }
+
     renderElement() {
-        const {ListItem = RXSelectionItem} = this.props;
-        return <div onClick={this.onClickHandler.bind(this)} ref="listRoot">
-            <List items={this.getFilteredOptions()} selectionManager={this.selectionManager}
-                  selection={this.state.__shadowValue} ListItem={ListItem}/>
-        </div>
+        const toRenderOptions = this.savedValue;
+        return <RXElementGroup onValueChange={this.onValueChangeHandler.bind(this)}>
+            {toRenderOptions.map((item, index) => {
+                return <RXDropdown options={this.getFilteredOptions()} showLabel={false}
+                                   name={item} key={item} value={item} ListItem={this.ListItem}>
+                </RXDropdown>
+            })}
+        </RXElementGroup>
     }
 }
 
-RXTextInputSet.defaultProps = {
+RXDropdownSet.defaultProps = {
     ...RXSelectionElement.defaultProps,
-    multiSelect:true
+    multiSelect: true
 }
 
 export default class InputSetPage extends SmartWrapper {
-    render(){
-        return <div className="input-set">This is page InputSet</div>
+    onValueChangeHandler(changed, fullObject) {
+        console.log(fullObject);
+    }
+
+    render() {
+        return <div className="input-set">
+            <RXForm onValueChange={this.onValueChangeHandler.bind(this)}>
+                <RXDropdownSet options={rxOptions} value={'1,2'} name="depts" label="Departments" exposeSelection={true}/>
+                <RXDropdownSet options={rxOptions} value={'1,2'} name="designations" label="Designations"/>
+            </RXForm>
+        </div>
     }
 }
