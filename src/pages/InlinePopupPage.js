14c6ec9a1e280dccc2b4157c8eb65314e81a8016
Ravi Hamsa <rhamsa@linkedin.com>
2018-02-01 00:44:06 +0530

    inline modal examples updated

--- src/pages/InlinePopupPage.js
+++ src/pages/InlinePopupPage.js
@@ -3,6 +3,15 @@
  */
 import React, {Component, PropTypes} from "react";
 import {SmartWrapper, InlinePopup, InlineButton, InlineBody} from 'react-starter-components';
+import {InlineModal, InlineModalBody, InlineModalButton} from 'react-starter-components';
+
+class SomethingButton extends Component {
+	render(){
+		return  <div style={{display:'inline-block', 'backgroundColor':'red'}}>
+			<button onClick={this.props.onClick}>button</button>
+		</div>
+	}
+}
 
 export default class InlinePopupPage extends SmartWrapper {
 
@@ -12,7 +21,7 @@ export default class InlinePopupPage extends SmartWrapper {
     }
 
     render(){
-        return <div className="popup-page">
+        return <div className="popup-page" style={{padding:'100px'}}>
             <InlinePopup>
                 <InlineButton>
                     <div>click me</div>
@@ -22,6 +31,86 @@ export default class InlinePopupPage extends SmartWrapper {
                 </InlineBody>
             </InlinePopup>
 
+
+	        <h1>Modal inside modal</h1>
+	        <InlineModal>
+		        <InlineModalButton>
+			        <SomethingButton />
+		        </InlineModalButton>
+		        <InlineModalBody  ref={popup=>this.myPopup=popup} name={"outside"}>
+			        <div>
+				        <p style={{whiteSpace:'nowrap'}}>somethingsssssssss extra test </p>
+				        <InlineModal >
+					        <InlineModalButton>
+						        <SomethingButton />
+					        </InlineModalButton>
+					        <InlineModalBody  ref={popup=>this.myPopup=popup} name={"inside"}>
+						        <div>This is the popup body</div>
+					        </InlineModalBody>
+				        </InlineModal>
+			        </div>
+		        </InlineModalBody>
+	        </InlineModal>
+
+
+	        <h1>Valign - top</h1>
+
+	        <InlineModal >
+		        <InlineModalButton>
+			        <SomethingButton />
+		        </InlineModalButton>
+		        <InlineModalBody  ref={popup=>this.myPopup=popup} valign="top">
+			        <div style={{whiteSpace:'nowrap'}}>This is the popup body</div>
+		        </InlineModalBody>
+	        </InlineModal>
+
+
+	        <h1>Halign - right</h1>
+
+
+	        <InlineModal >
+		        <InlineModalButton>
+			        <SomethingButton />
+		        </InlineModalButton>
+		        <InlineModalBody  ref={popup=>this.myPopup=popup} halign="right">
+			        <div style={{whiteSpace:'nowrap'}}>This is the popup body</div>
+		        </InlineModalBody>
+	        </InlineModal>
+
+	        <h1>Halign - center</h1>
+
+	        <InlineModal >
+		        <InlineModalButton>
+			        <SomethingButton />
+		        </InlineModalButton>
+		        <InlineModalBody  ref={popup=>this.myPopup=popup} halign="center">
+			        <div style={{whiteSpace:'nowrap'}}>This is the popup body</div>
+		        </InlineModalBody>
+	        </InlineModal>
+
+
+	        <h1>bodyPosition - up</h1>
+
+	        <InlineModal >
+		        <InlineModalButton>
+			        <SomethingButton />
+		        </InlineModalButton>
+		        <InlineModalBody  ref={popup=>this.myPopup=popup} bodyPosition="up">
+			        <div style={{whiteSpace:'nowrap'}}>
+				        <ul>
+					        <li>Item 1 asdf asd f asdf a sdf asd fa sdf asd f</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+					        <li>Item 1</li>
+				        </ul>
+			        </div>
+		        </InlineModalBody>
+	        </InlineModal>
         </div>
+
     }
 }
